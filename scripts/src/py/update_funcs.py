fe3754a4e3b5ff0b55c79f7212664019976d01a8
nrvnrv <nrvnsk@mail.ru>
2023-07-13 10:27:57 +0300

    upd pathes-upd script, upd jsons forms (cfg+pathes)
    - upd pathes-upd scripts - pathes from root, added contents from flag files
    - flag files contents list of includin files
    - upd of cfg.josn
    - upd form of pathes.josn (pathes from root)

--- scripts/src/py/update_funcs.py
+++ scripts/src/py/update_funcs.py
@@ -1,5 +1,25 @@
 import os
 import json
+import re
+
+
+def getFileList(filepath):
+
+    """
+    Сформировать 
+
+    :param filepath: То что будет записано в json
+    :type filepath: dict
+    :return: res
+    :rtype: dict
+    """
+
+    fileList = []
+    reg = re.compile('[^a-zA-Z0-9~@#$%^-_(){}'':/\.` ]')
+    with open(filepath, "r") as f:
+        for lines in f:
+            fileList.append(reg.sub('', lines))
+    return fileList
 
 
 def get_dir_of_searching_file(folder: str, searching_file: str):
@@ -28,13 +48,38 @@ def get_dir_of_searching_file(folder: str, searching_file: str):
     return searched_dirs
 
 
-def form_artifacts_list(base_folder: str, search_dict: dict) -> list:
+def update_res_dirs(res, field_lvl_1, fields):
+
+    """
+    Сформировать словарь res для функции update_pathes.
+    Чтобы в словаре было несколько словарей.
+
+    :param res: То что будет записано в json
+    :type res: dict
+    :param field_lvl_1: Вставляемое в res поле 
+    :type field_lvl_1: str
+    :param fields: Вставляемый в res словарь
+    :type fields: dict
+    :return: res
+    :rtype: dict
+    """
+
+    if field_lvl_1 in res:
+        res[field_lvl_1].update(fields)
+    else:
+        res.update({ field_lvl_1: fields })
+        
+    return res
+
+def form_artifacts_list(root_folder: str, submod_folder: str, search_dict: dict) -> list:
 
     """
     Сформировать dict для функции update_pathes
 
-    :param base_folder: Путь к корневой папке репозитория подмодулей
-    :type base_folder: str
+    :param root_folder: Путь к корневой папке репозитория подмодулей
+    :type root_folder: str
+    :param submod_folder: Путь к папке подмодулей
+    :type submod_folder: str
     :param search_dict: [ключ - искомый объект]: значение - флаг искомого объекта. Флаг - файл, который должен находиться в папке с искомым объектом
     :type search_dict: dict
     :return: Содержимое результирующего файла result_json_path
@@ -43,48 +88,76 @@ def form_artifacts_list(base_folder: str, search_dict: dict) -> list:
 
     # finded_pathes = {} # найденные пути до папок объектов модулей
     # for searchin_obj in search_dict:
-    #     finded_pathes[searchin_obj] = get_dir_of_searching_file(base_folder, search_dict[searchin_obj])
+    #     finded_pathes[searchin_obj] = get_dir_of_searching_file(submod_folder, search_dict[searchin_obj])
     # print(json.dumps(finded_pathes, indent = 4))
 
     # modules_dict = match_module_artifact(finded_pathes)
     # form_json(result_json_path, modules_dict)
 
-    # Список всех файлов всех поддиректорий папки base_folder
-    subpathes_list = os.walk(base_folder) 
+    # Список всех файлов всех поддиректорий папки submod_folder
+    subpathes_list = os.walk(submod_folder) 
     searched_dirs = [] # формируемый список путей к файлам searching_file
+
     # поиск путей ко всем файлам searching_file
     for address, dirs, files in subpathes_list:
+        address = address[len(root_folder):] # делаем путь относительно корня
         for name in files:
             for obj in search_dict:
                 if (search_dict[obj] == name):
-                    searched_dirs.append({'obj': obj, 'path': address})
+                    print(address)
+                    searched_dirs.append({
+                        'obj': obj, 
+                        'path': address
+                    })
 
-
-    result_pathes = []
+    # парс путей
+    result_pathes = {}
     for i in range(len(searched_dirs)):
         for j in range(len(searched_dirs)):
             if ((i != j) and (searched_dirs[i] != None) and (searched_dirs[j] != None)
-                and (searched_dirs[i]['obj'] in searched_dirs[j]['obj'])
-                and (searched_dirs[i]['path'] in searched_dirs[j]['path'])):
-                    module_name = os.path.split(searched_dirs[i]['path'])[1]
-                    result_pathes.append({module_name: {
-                        searched_dirs[i]['obj']: searched_dirs[i]['path'],
-                        searched_dirs[j]['obj']: searched_dirs[j]['path']
-                    }})
-                    searched_dirs[i] = None
-                    searched_dirs[j] = None
+                    and (searched_dirs[i]['obj'] in searched_dirs[j]['obj'])
+                    and (searched_dirs[i]['path'] in searched_dirs[j]['path'])):
+                
+                # for subpath in os.path.normpath(searched_dirs[i]['path']).split('\\'):
+                #     if():
+                #         ...
+                # лучше исправить на алгоритмический способ 
+                language = os.path.normpath(searched_dirs[i]['path']).split('\\')[1]
+                module_name = '.'.join(os.path.normpath(searched_dirs[i]['path']).split('\\')[2:])
+                
+                result_pathes = update_res_dirs(result_pathes, language, {
+                    module_name: {
+                        searched_dirs[i]['obj']: {
+                            "path": searched_dirs[i]['path'],
+                            "file": getFileList(os.path.join(root_folder, searched_dirs[i]['path'], search_dict[searched_dirs[i]['obj']]))
+                        },
+                        searched_dirs[j]['obj']: {
+                            "path": searched_dirs[j]['path'],
+                            "file": getFileList(os.path.join(root_folder, searched_dirs[j]['path'], search_dict[searched_dirs[j]['obj']]))
+                        },
+                        
+                }})
+                searched_dirs[i] = None
+                searched_dirs[j] = None
 
     for i in range(len(searched_dirs)):
         if (searched_dirs[i] != None):
+            language = os.path.normpath(searched_dirs[i]['path']).split('\\')[2]
             module_name = os.path.split(searched_dirs[i]['path'])[1]
-            result_pathes.append({module_name: {
-                searched_dirs[i]['obj']: searched_dirs[i]['path'],
+            result_pathes = update_res_dirs(result_pathes, language, {
+                module_name: {
+                    searched_dirs[i]['obj']: {
+                            "path": searched_dirs[i]['path'],
+                            "file": getFileList(os.path.join(root_folder, searched_dirs[i]['path'], search_dict[searched_dirs[i]['obj']]))
+                        },
             }})
             searched_dirs[i] = None
+
     return result_pathes
 
 
 def form_json_file(json_to_save, json_path: str):
+
     """
     Сохранить json в json файл
 
@@ -94,6 +167,7 @@ def form_json_file(json_to_save, json_path: str):
     :type json_path: str
     :return: None
     """
+
     with open(json_path, "w", encoding="utf-8") as json_file:
         json.dump(json_to_save, json_file)
         json_file.close()
--- scripts/update_pathes.py
+++ scripts/update_pathes.py
@@ -3,7 +3,7 @@ import json
 from src.py.update_funcs import form_artifacts_list, form_json_file
 
 
-def update_pathes(base_folder: str, search_dict: dict, result_json_path_list: list) -> list:
+def update_pathes(root_folder: str, submod_folder: str, search_dict: dict, result_json_path_list: list) -> list:
 
     """
     Сформировать конфигурационый path-файл (json) артефактов модулей - 
@@ -14,8 +14,10 @@ def update_pathes(base_folder: str, search_dict: dict, result_json_path_list: li
         }
     }
 
-    :param base_folder: Путь к корневой папке репозитория подмодулей
-    :type base_folder: str
+    :param root_folder: Путь к корневой папке репозитория подмодулей
+    :type root_folder: str
+    :param submod_folder: Путь к папке с подмодулями
+    :type submod_folder: str
     :param search_dict: [ключ - искомый объект]: значение - флаг искомого объекта. Флаг - файл, который должен находиться в папке с искомым объектом
     :type search_dict: dict
     :param result_json_path_list: Пути к формируемым path-файлам  
@@ -25,7 +27,7 @@ def update_pathes(base_folder: str, search_dict: dict, result_json_path_list: li
     """
 
     # формируем словарь (формат см описание ф-ии)
-    artifacts_list = form_artifacts_list(base_folder, search_dict)
+    artifacts_list = form_artifacts_list(root_folder, submod_folder, search_dict)
     # сохраняем полученный словарь в указанные файлы
     for result_json_path in result_json_path_list:
         form_json_file(artifacts_list, result_json_path)
@@ -46,41 +48,39 @@ def update_pathes_main(repo_root: str, submodules_conf_json_path: str, result_js
     :type result_json_path_list: list
     :return: Содержимое результирующего файла result_json_path (см update_pathes)
     :rtype: list
-    """
-    
+    """    
 
     # читаем конфиги 
     with open(submodules_conf_json_path) as f:
         SUBMODULES_CONF = json.load(f)
 
     # путь к папке с подмодулями
-    submod_folder = repo_root + SUBMODULES_CONF['PATH']['submodules_folder']
+    submod_folder = repo_root + SUBMODULES_CONF['submodules_folder']
     current_script_folder = os.path.dirname(os.path.abspath(__file__)) # папка текущего скрипта
     current_exec_folder = os.getcwd() # папка откуда запускается скрипт
     
     # задаем структуру того что будем искать
     # [ключ - искомый объект]: значение - файл-флаг искомого объекта
     # SEARCH_DICT = {
-    #     "component": SUBMODULES_CONF['PATH']['FLAG']['component'],
-    #     "module": SUBMODULES_CONF['PATH']['FLAG']['module'],
-    #     "module_include": SUBMODULES_CONF['PATH']['FLAG']['module_include'],
-    #     "module_src": SUBMODULES_CONF['PATH']['FLAG']['module_src']
+    #     "component": SUBMODULES_CONF['PATH']['SUBMODS_FLAGS']['component'],
+    #     "module": SUBMODULES_CONF['PATH']['SUBMODS_FLAGS']['module'],
+    #     "module_include": SUBMODULES_CONF['PATH']['SUBMODS_FLAGS']['module_include'],
+    #     "module_src": SUBMODULES_CONF['PATH']['SUBMODS_FLAGS']['module_src']
     # }
     # определяется в конифгах 
-    SEARCH_DICT = SUBMODULES_CONF['PATH']['FLAG']
+    SEARCH_DICT = SUBMODULES_CONF['SUBMODS_FLAGS']
 
     # сохранение в repo_root если список result_json_path_list пустой
     # if (result_json_path_list == []): 
     #     result_json_path_list = [repo_root + SUBMODULES_CONF['PATH']['submod_pathes_json']]
     
     # также сохраняем в папку конфигов
-    result_json_path_list.append(
-        SUBMODULES_CONF['PATH']['conf_folder'] + SUBMODULES_CONF['PATH']['submod_pathes_json']
-    )
+    result_json_path_list.append(repo_root + SUBMODULES_CONF['submod_pathes_json'])
 
     # обновляем пути к искомым объектам
     artifacts_list = update_pathes(
-        base_folder = submod_folder, # где искать
+        root_folder = repo_root, # корень проекта
+        submod_folder = submod_folder, # где искать
         search_dict = SEARCH_DICT, # что искать
         result_json_path_list = result_json_path_list # куда сохранять
     )
